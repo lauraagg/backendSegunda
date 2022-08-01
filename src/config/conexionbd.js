5a7a015e4c5ed15d9b9db80310a138edea39d160
alexis1877 <95194034+alexis1877@users.noreply.github.com>
2022-07-31 22:52:34 -0500

    Add files via upload

--- src/config/conexionbd.js
+++ src/config/conexionbd.js
@@ -5,7 +5,7 @@ const conecta = mysql.createConnection({
     user:"root",
     password:"123456",
     port:"3306",
-    database:"proyecto"
+    database:"appcarrito"
 });
 conecta.connect();
 module.exports = conecta;
--- src/rutas/rutas.js
+++ src/rutas/rutas.js
@@ -25,6 +25,7 @@ ruta.post("/usuario/registrarUsuario", (req, res) => {
 });
 
 ruta.post("/usuario/borrar", (req, res) => {
+  
   const { id } = req.body;
   let query = "DELETE  FROM `appcarrito`.`usuario`  WHERE id_usuario = ?";
   consulta.query(query, [id], (error, rows) => {
@@ -74,17 +75,13 @@ ruta.post("/login", (req, res) => {
   consulta.query(query, (err, resp) => {
     if (!err) {
       pass = resp[0].password;
-      console.log(password);
-      console.log(pass);
       if (password == pass) {
-        console.log("se cumple");
-        console.log(pass);
         res.json(resp[0]);
       } else {
         res.json(err);
       }
     } else {
-      console.log(err);
+      res.json("no se encuentra el correo");
     }
   });
 });
@@ -110,6 +107,170 @@ ruta.post("/usuario/update", (req, res) => {
       res.json(err);
     }
   });
+});
+
+//productos
+//Mostrar todos los productos
+
+ruta.get('/productos', (req, res) => {
+  let query = "SELECT P.id, P.nombre_producto, P.precio_producto, P.cantidad, P.desc, SC.nombre AS Nombre_Sub_Categoria, C.nombre AS Cat, IM.url FROM producto_subcategoria PSC INNER JOIN productos P ON PSC.id_productos = P.id INNER JOIN sub_categoria SC ON PSC.id_sub_categoria = SC.id_sub_categoria INNER JOIN categoria C ON SC.id_categoria = C.id_categoria INNER JOIN imagen IM ON IM.id_producto = P.id";
+
+  consulta.query(query, (err, rows) => {
+    if (!err) {
+      res.json(rows);
+    }
+    else {
+      console.log(err);
+    }
+  })
+});
+
+//Obtener productos por ID
+
+ruta.get('/productos/:id', (req, res) => {
+  const { id } = req.params;
+  let query = "SELECT P.id, P.nombre_producto, P.precio_producto, P.cantidad, P.desc, SC.nombre AS Nombre_Sub_Categoria, C.nombre AS Cat, IM.url FROM producto_subcategoria PSC INNER JOIN productos P ON PSC.id_productos = P.id INNER JOIN sub_categoria SC ON PSC.id_sub_categoria = SC.id_sub_categoria INNER JOIN categoria C ON SC.id_categoria = C.id_categoria INNER JOIN imagen IM ON IM.id_producto = P.id WHERE id = ?";
+  consulta.query(query, [id], (error, rows) => {
+    if (Object.keys(rows).length !== 0) {
+      return res.json(rows[0]);
+    } else {
+      res.status(400).json({ text: 'no existe el producto' })
+    }
+  })
+});
+
+//Crando productos
+
+ruta.post('/productos/crear', (req, res) => {
+
+  const {nombre_producto, precio_producto, cantidad, desc, id_sub_categoria, url } = req.body;
+  
+  const query = `
+      call insertarDatos(?, ?, ?, ?, ?, ?);
+  `;
+  consulta.query(query, [nombre_producto, precio_producto, cantidad, desc, id_sub_categoria, url], (error, rows, fields) => {
+    if (!error){
+      res.json({Status: 'Producto Creado'});
+    }
+    else{
+      console.log(error);
+    }
+  })
+});
+
+//Actualizar productos
+
+ruta.put('/productos/actualizar/:id', (req, res) => {
+  const {nombre_producto, precio_producto, cantidad, desc, id_sub_categoria, url } = req.body;
+  const {id} = req.params;
+  const query = `
+      call modificarDatos(?, ?, ?, ?, ?, ?, ?);
+  `;
+
+  consulta.query(query, [id, nombre_producto, precio_producto, cantidad, desc, id_sub_categoria, url], (error, rows, fields) => {
+    if (!error){
+      res.json("producto actualizado");
+    }
+    else{
+      console.log(error);
+    }
+  })
+})
+
+
+//Borrar productos
+
+ruta.delete('/productos/eliminar/:id', (req, res) => {
+  const { id } = req.params;
+  let query = "call eliminarDatos(?);";
+  consulta.query(query, [id], (error, rows) => {
+    if (!error) {
+      res.json('Producto eliminado');
+    }
+    else {
+      console.log(error);
+    }
+  })
+});
+
+
+// Termina productos
+
+//  CONSULTAR TICKET
+//http://localhost:4200/ticket/consulta/1
+ruta.get('/ticket/consulta/:id', (req, res) => {
+  const { id } = req.params;
+  //let sql = "SELECT venta.id_venta, venta.id_ticket, venta.cantidad, nombre_producto, categoria_producto, precio_producto, descripbr_producto, imagen FROM venta INNER JOIN productos ON venta.id_producto = productos.id -- tabla.FK = tabla.PK INNER JOIN ticket ON venta.id_ticket = ticket.id_ticket;";
+  let sql = "SELECT venta.id_venta, usuario.nombre, venta.id_ticket, venta.cantidad, nombre_producto, precio_producto, productos.desc, imagen FROM venta INNER JOIN productos ON venta.id_producto = productos.id INNER JOIN ticket ON venta.id_ticket = ticket.id_ticket INNER JOIN usuario ON ticket.id_usuario =usuario.id_usuario WHERE ticket.id_ticket = ?";
+  consulta.query(sql, [id], (err, rows) => {
+      if (!err) {
+          res.json(rows);
+      } else {
+          console.log(err);
+      }
+
+  })
+});
+
+//ELIMINAR
+//DELETE FROM `appcarrito`.`venta` WHERE id_venta = 9;
+//elimina venta, no ticket
+ruta.delete('/ticket/eliminar/:id', (req, res) => {
+  const { id } = req.params;
+  let sql = "DELETE FROM `appcarrito`.`venta` WHERE id_venta = ?";
+  consulta.query(sql, [id], (error, rows) => {
+      if (!error) {
+          res.json(rows);
+      } else {
+          console.log(error);
+      }
+  })
+});
+
+// -------------------------------
+
+//INSERTAR PRODUCTO EN TICKET
+//  NOTA: colocar mismas variables que en postman
+//http://localhost:4200/ticket/insertar
+ruta.post('/ticket/insertar', (req, res) => {
+  const { id_ticket, id_producto, cantidad } = req.body;
+  let sql = "call insertar_en_ticket('" + id_ticket + "', '" + id_producto + "', '" + cantidad + "')";
+  consulta.query(sql, (err, rows) => {
+      if (!err) {
+          res.json(rows);
+      } else {
+          console.log(err);
+      }
+
+  })
+});
+
+//MODIFICAR CANTIDAD
+ruta.post('/ticket/modificacant', (req, res) => {
+  const { id_venta, cantidad } = req.body;
+  let sql = "call modificar_cant('" + id_venta + "', '" + cantidad + "')";
+  consulta.query(sql, (err, rows) => {
+      if (!err) {
+          res.json(rows);
+      } else {
+          console.log(err);
+      }
+
+  })
+});
+
+ruta.post('/generaticket', (req, res) => {
+  const { id } = req.body;
+  console.log(req.body);
+  let sql = "INSERT INTO `appcarrito`.`ticket` (`id_usuario`) VALUES ('" + id + "')";
+  consulta.query(sql, (err, rows) => {
+      if (!err) {
+          res.json(rows.insertId);
+      } else {
+          console.log(err);
+      }
+
+  })
 });
 
 module.exports = ruta;
